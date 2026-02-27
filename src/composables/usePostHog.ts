e0d227bc2174bb80ddc06afb9e116e8e09a717c7
DevArtech <iamhaile222@gmail.com>
2026-02-27 01:28:33 -0600

    fix: remove ph key

--- src/composables/usePostHog.ts
+++ src/composables/usePostHog.ts
@@ -2,7 +2,7 @@ import posthog from 'posthog-js'
 
 const POSTHOG_API_KEY =
   import.meta.env.VITE_POSTHOG_API_KEY ||
-  'phc_wva5vQhVaZRCEUh691CYejTmZK60EdyqkRFToNIBVl2'
+  '' // To be set via environment variable
 const POSTHOG_API_HOST = import.meta.env.VITE_POSTHOG_API_HOST || 'https://us.i.posthog.com'
 const POSTHOG_DEFAULTS = import.meta.env.VITE_POSTHOG_DEFAULTS || '2025-11-30'
 
